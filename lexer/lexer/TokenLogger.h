bd3fd24c28ac3e6144f3aad53e0f542f6cba91a2
Firdavsi <firdavsinurov326@gmail.com>
2023-02-25 15:16:57 +0300

    lexer

---
+++ lexer/lexer/TokenLogger.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include <iostream>
+#include <functional>
+
+#include "TokenWithContext.h"
+
+class TokenLogger
+{
+public:
+    static void SetOutputStream(std::ostream& out);
+
+    static void Log(const std::vector<TokenWithContext>&);
+
+private:
+    static void LogOne(const TokenWithContext&);
+
+    inline static std::reference_wrapper<std::ostream> m_out = std::cout;
+};
